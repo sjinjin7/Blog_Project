2d87426eddb7181913885eca324e9159c7eae818
SeoJin Kim <sjinjin7@gmail.com>
2021-05-17 16:35:46 +0900

    [Spring][쇼핑몰 프로젝트][24] 상품 이미지 업로드(이미지 정보 뷰 반환 - 1) - 6
    https://kimvampa.tistory.com/220

---
+++ VamPa/src/main/java/com/vam/model/AttachImageVO.java
@@ -0,0 +1,56 @@
+package com.vam.model;
+
+public class AttachImageVO {
+
+	/* 경로 */
+	private String uploadPath;
+	
+	/* uuid */
+	private String uuid;
+	
+	/* 파일 이름 */
+	private String fileName;
+	
+	/* 상품 id */
+	private int bookId;
+
+	public String getUploadPath() {
+		return uploadPath;
+	}
+
+	public void setUploadPath(String uploadPath) {
+		this.uploadPath = uploadPath;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	@Override
+	public String toString() {
+		return "AttachImageVO [uploadPath=" + uploadPath + ", uuid=" + uuid + ", fileName=" + fileName + ", bookId="
+				+ bookId + "]";
+	}
+	
+	
+}

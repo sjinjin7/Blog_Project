b834a3ca40c8e11b2f12d184af8152955cbffa0c
SeoJin Kim <sjinjin7@gmail.com>
2021-06-02 01:29:10 +0900

    [Spring][쇼핑몰 프로젝트][26] 업로드 이미지 삭제 - 1
    https://kimvampa.tistory.com/225

--- VamPa/src/main/java/com/vam/controller/AdminController.java
+++ VamPa/src/main/java/com/vam/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.vam.controller;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -403,6 +404,42 @@ public class AdminController {
 		ResponseEntity<List<AttachImageVO>> result = new ResponseEntity<List<AttachImageVO>>(list, HttpStatus.OK);
 		
 		return result;
+	}
+	
+	/* 이미지 파일 삭제 */
+	@PostMapping("/deleteFile")
+	public ResponseEntity<String> deleteFile(String fileName){
+		
+		logger.info("deleteFile........" + fileName);
+		
+		File file = null;
+		
+		try {
+			/* 썸네일 파일 삭제 */
+			file = new File("c:\\upload\\" + URLDecoder.decode(fileName, "UTF-8"));
+			
+			file.delete();
+			
+			/* 원본 파일 삭제 */
+			String originFileName = file.getAbsolutePath().replace("s_", "");
+			
+			logger.info("originFileName : " + originFileName);
+			
+			file = new File(originFileName);
+			
+			file.delete();
+			
+			
+		} catch(Exception e) {
+			
+			e.printStackTrace();
+			
+			return new ResponseEntity<String>("fail", HttpStatus.NOT_IMPLEMENTED);
+			
+		} // catch
+		
+		return new ResponseEntity<String>("success", HttpStatus.OK);
+		
 	}
 	
 }

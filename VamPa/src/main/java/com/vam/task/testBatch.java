670f3a5fa2631fc989ac196d6d9257f54bf77c97
SeoJin Kim <sjinjin7@gmail.com>
2021-07-26 00:33:11 +0900

    [Spring][쇼핑몰 프로젝트][29] 상품 이미지 수정 - 4(배치 프로그램 적용 1)
    https://kimvampa.tistory.com/238

---
+++ VamPa/src/main/java/com/vam/task/testBatch.java
@@ -0,0 +1,22 @@
+package com.vam.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.log4j.Log4j;
+
+@Component
+@Log4j
+public class testBatch {
+
+
+	@Scheduled(cron = "0 * * * * *")
+	public void testMethod() throws Exception{
+		
+		log.warn("배치 실행 테스트.......");
+		log.warn("===================================");
+
+		
+	}
+	
+}
